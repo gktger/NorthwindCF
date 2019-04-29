0493af6230cef6d73a5ae75c2a82401be8af365e
Goktug Ercaliskan <goktug.ercaliskan@BK-LAB204-IS025>
2019-04-29 14:44:29 +0300

    web form eklendi

---
+++ NorthWindCF.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using NorthWindCF.BLL;
+using NorthWindCF.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWindCF.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+           
+
+            return View();
+        }
+
+        public ActionResult Kaydet()
+        {
+
+            string musteriAdi = Request.Params["txt_MusteriAdi"];
+
+            Customer customer = new Customer();
+            customer.City = "city";
+            customer.Address = "";
+            customer.CompanyName = "";
+            customer.ContactName = "";
+            customer.ContactTitle = "";
+            customer.Country = "";
+            customer.Fax = "";
+            customer.Phone = "";
+            customer.PostalCode = "";
+            customer.Region = "";
+            customer.CustomerID = musteriAdi.Substring(0, 5).ToUpper();
+
+            int eklendiMi = CustomerBLL.AddCustomer(customer);
+            return RedirectToAction("About");
+ 
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NorthWindCF.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NorthWindCF.WebUI.MvcApplication" Language="C#" %>
